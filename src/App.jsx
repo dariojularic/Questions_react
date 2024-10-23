f425821e5fe909cc499611525c98a2df00be5f97
Dario Jularic <dariojularic0@gmail.com>
2024-10-23 17:48:54 +0200

    Refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// import { useEffect, useState } from 'react'
 import './App.css'
 import questions from './data'
 import Question from './Question'
--- src/Question.jsx
+++ src/Question.jsx
@@ -3,19 +3,19 @@ import './Question.css'
 import PropTypes from 'prop-types';
 
 function Question(props) {
-    const [isOpen, setIsOpen] = useState(false);
-    const {title, info} = props;
-    return (
-        <li className="list-item">
-          <div className="question-container">
-            <h3 className="question-title">{title}</h3>
-            <button className="open-close-btn" onClick={() => setIsOpen(!isOpen)}>
-              {isOpen ? "-" : "+"}
-            </button>
-          </div>
-            {isOpen ? (<p className="question-info"> {info} </p>) : null}
-        </li>
-    )
+  const [isOpen, setIsOpen] = useState(false);
+  const {title, info} = props;
+  return (
+    <li className="list-item">
+      <div className="question-container">
+        <h3 className="question-title">{title}</h3>
+        <button className="open-close-btn" onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? "-" : "+"}
+        </button>
+      </div>
+        {isOpen ? (<p className="question-info"> {info} </p>) : null}
+    </li>
+  )
 }
 
 Question.propTypes = {
